df0c0f44542456cf5a203696034512bd0b5fe034
READINGROOM\leslie.khumalo <leslie.khumalo@readingroom.com>
2018-03-05 09:25:45 +0000

    Initial build

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+/*
+    ./webpack.config.js
+*/
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './client/index.html',
+  filename: 'index.html',
+  inject: 'body'
+})
+module.exports = {
+  entry: __dirname + '/client/index.js',
+  output: {
+    path: __dirname + '/dist',
+    filename: 'index_bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.jsx$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  plugins: [HtmlWebpackPluginConfig]
+};
